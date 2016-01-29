0e70886744b21004757e4d629e9b0f91db696d67
Radoslav Gospodinov <rad.gospodinov@gmail.com>
2016-01-29 15:14:49 +0200

    added total price for cart and adjusted methods to calc total price

--- FoodDeliverySystem/src/basic/Cart.java
+++ FoodDeliverySystem/src/basic/Cart.java
@@ -4,23 +4,29 @@ import java.util.ArrayList;
 
 public class Cart {
 	ArrayList<Meal> products;
-	
-
+	double totalPrice;
 
 	public ArrayList<Meal> getBasket() {
 		return this.products;
 	}
-	
-	void addToproducts(Meal product){
+
+	void addToproducts(Meal product) {
 		this.products.add(product);
+		this.totalPrice += product.getPrice();
 	}
-	void removeFromproducts(Meal meal){
+
+	void removeFromproducts(Meal meal) {
 		this.products.remove(meal);
+		this.totalPrice -= meal.getPrice();
 	}
-	
-	void emptyCart(){
+
+	void emptyCart() {
 		this.products.clear();
+		this.totalPrice = 0;
 	}
-	
-	
+
+	double getTotalPrice() {
+		return totalPrice;
+	}
+
 }
