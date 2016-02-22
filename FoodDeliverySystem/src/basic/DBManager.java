ca4fa7f3d92ac137c6b57cce7ae6cbd3265509d3
Simeon Markov <simeon.markov1994@gmail.com>
2016-02-22 20:25:08 +0200

    Old try/catch blocks replaced with try-with-resources for better readability + fix for schema relative path (why does it work with only 1 ../  ? )

--- FoodDeliverySystem/src/basic/DBManager.java
+++ FoodDeliverySystem/src/basic/DBManager.java
@@ -10,72 +10,70 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class DBManager {
-	
+
 	private static DBManager instance;
-	
-	private DBManager() throws FileNotFoundException, IOException, SQLException {
-		try{
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-			} catch (ClassNotFoundException e) {
-				System.out.println("Error:No such driver imported!");
-			}
-			final String url = "jdbc:mysql://localhost:3306/hr";
-			final String user = "root";
-			final String pass = "root";
-			Connection conn = DriverManager.getConnection(url,user,pass);
-			executeSqlScript(conn, new File("../../../DB/fd-db-scheme.sql"));
+
+	private DBManager() {
+		final String url = "jdbc:mysql://localhost:3306/hr?useSSL=false";
+		final String user = "root";
+		final String pass = "root";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error:No such driver imported!");
 		}
-		catch(SQLException e){
-			e.printStackTrace();
+
+		try (Connection conn = DriverManager.getConnection(url, user, pass);) {
+			executeSqlScript(conn, new File("../DB/fd-db-scheme.sql"));
+		} catch (SQLException e) {
+			System.out.println("Error:Either something wrong in the connection object or wrong query syntax!");
 		}
+
 	}
-	
-	public static DBManager getInstance() throws ClassNotFoundException, SQLException, FileNotFoundException, IOException{
-		if(instance == null){
+
+	public static DBManager getInstance() throws ClassNotFoundException,
+			SQLException, FileNotFoundException, IOException {
+		if (instance == null) {
 			instance = new DBManager();
 		}
 		return instance;
 	}
-	
+
 	public void executeSqlScript(Connection conn, File inputFile) {
 
-	    // Delimiter
-	    String delimiter = ";";
+		// Delimiter
+		String delimiter = ";";
 
-	    // Create scanner
-	    Scanner scanner;
-	    try {
-	        scanner = new Scanner(inputFile).useDelimiter(delimiter);
-	    } catch (FileNotFoundException e1) {
-	        e1.printStackTrace();
-	        return;
-	    }
+		// Create scanner
+		try (Scanner scanner = new Scanner(inputFile).useDelimiter(delimiter);) {
 
-	    // Loop through the SQL file statements 
-	    Statement currentStatement = null;
-	    while(scanner.hasNext()) {
+			// Loop through the SQL file statements
+			Statement currentStatement = null;
+			while (scanner.hasNext()) {
 
-	        // Get statement 
-	        String rawStatement = scanner.next() + delimiter;
-	        try {
-	            // Execute statement
-	            currentStatement = conn.createStatement();
-	            currentStatement.execute(rawStatement);
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        } finally {
-	            // Release resources
-	            if (currentStatement != null) {
-	                try {
-	                    currentStatement.close();
-	                } catch (SQLException e) {
-	                    e.printStackTrace();
-	                }
-	            }
-	            currentStatement = null;
-	        }
-	    }
-	scanner.close();
+				// Get statement
+				String rawStatement = scanner.next() + delimiter;
+				try {
+					// Execute statement
+					currentStatement = conn.createStatement();
+					currentStatement.execute(rawStatement);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} finally {
+					// Release resources
+					if (currentStatement != null) {
+						try {
+							currentStatement.close();
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+					}
+					currentStatement = null;
+				}
+			}
+		} catch (FileNotFoundException e1) {
+			System.out.println("No such file in this directory!");
+		}
 	}
 }
