6317eff2d2320c41f156464999da7d3cd8037d03
Simeon Markov <simeon.markov1994@gmail.com>
2016-03-07 18:06:19 +0200

    Also,just added the View package in src with all the basic Java classes so far(btw,there is one servlet in controller - Login,that does nothing yet.Ignore it- I'll work on it later)

---
+++ FoodDeliveryWholeProject/src/view/Meal.java
@@ -0,0 +1,98 @@
+package view;
+
+import java.util.ArrayList;
+
+public class Meal implements Comparable<Meal> {
+	
+	public enum MealType {
+	     SALAD,SOUP,PASTA,PIZZA,ALAMINUT,BBQ,FISH,BREAD,SOUCE,DESERT,DRINKS,VEGETERIAN,OSNOVNO,
+	     // POD VIDOVE ZA NQKOI OT IASTIATA S VID MESO ILI BBQ
+	     PORK,CHICKEN,BEEF,DUCK,
+	     // POD VID ZA PASTA
+	     SPAGHETTI,LASAGNA,
+	     // POD VID DRINKS
+	     BEER,WINE,STRONG_ALCOHOL,NON_ALCOHOL
+	     
+	     
+	     // TODO: MORE TO BE ADDED
+	    
+	}
+	
+	private String name;
+	private double price;
+	private MealType category;
+	private double rating;
+	private int timesRated;
+	private ArrayList<Ingredient> ingredients;
+	
+
+	public Meal(String name, double price, MealType category) {
+		this.name = name;
+		this.price = price;
+		this.category = category;
+		this.timesRated = 0;
+		this.ingredients = new ArrayList<Ingredient>();
+	}
+
+	public void setTimesRated() {
+		this.timesRated++;
+	}
+
+	public double getRating() {
+		return this.rating / this.timesRated;
+	}
+
+	public void setRating(double rating) {
+		this.rating += rating;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<Ingredient> getIgredients(){
+		return ingredients;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public MealType getCategory() {
+		return category;
+	}
+
+	public void setCategory(MealType category) {
+		this.category = category;
+	}
+
+	@Override
+	public int compareTo(Meal o) {
+		return (int) (price - o.price);
+	}
+
+	public String toString() {
+		StringBuilder allIngredientsWithNames = new StringBuilder("[");
+		if(!ingredients.isEmpty()){
+			for(int i = 0; i < ingredients.size() - 1; i++){
+				allIngredientsWithNames.append(ingredients.get(i).getIngredientName() + ",");
+			}
+			allIngredientsWithNames.append(ingredients.get(ingredients.size() - 1).getIngredientName()); 
+		}
+		allIngredientsWithNames.append("]");
+		return name + "\n Category: " + category + "\n Ingredients: " + allIngredientsWithNames + "\n Price: " + price;
+	}
+
+	public void addIngredients(Ingredient product) {
+		this.ingredients.add(product);
+	}
+
+}
---
+++ FoodDeliveryWholeProject/src/view/Order.java
@@ -0,0 +1,35 @@
+package view;
+
+import java.text.*;
+import java.util.*;
+
+public class Order {
+	private ArrayList<Meal> meals;
+	private String date;
+	private double price;
+
+	public Order(ArrayList<Meal> meals, double price) {
+		this.meals = meals;
+		setDate();
+		this.price = price;
+	}
+
+	public ArrayList<Meal> getMeals() {
+		return meals;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		this.date = dateFormat.format(date);
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+}
